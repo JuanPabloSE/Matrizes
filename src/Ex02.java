dca2976bdfe56cb4eef806ad6a838c72e7c4a8b2
JuanPabloSE <juancontas@protonmail.com>
2026-01-14 20:23:53 -0300

    Commit Message

---
+++ src/Ex02.java
@@ -0,0 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int [] [] mat = new int [N][N];
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("DIAGONAL PRINCIPAL: ");
+        for (int i = 0; i < mat.length; i++) {
+            System.out.print(mat[i][i] + " ");
+        }
+        System.out.println();
+
+        int cont = 0;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] < 0) {
+                    cont++;
+                }
+            }
+        }
+
+        System.out.println("QUANTIDADE DE NEGATIVOS = " + cont);
+
+        sc.close();
+    }
+}
