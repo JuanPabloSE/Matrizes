3ce2d00e234621746db341c079ed5ce98d176789
JuanPabloSE <juancontas@protonmail.com>
2026-01-15 14:31:24 -0300

    Commit Message

---
+++ src/ExProposto02.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExProposto02 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int [] [] matriz = new int [N][N];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < matriz.length; i++) {
+            int soma = 0;
+            for (int j = 0; j < matriz[i].length; j++) {
+                soma += matriz[i][j];
+            }
+            System.out.println(soma);
+        }
+
+        sc.close();
+    }
+}
