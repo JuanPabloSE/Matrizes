a62624911896a2c67a5d2dd6e34fd933fd870c01
JuanPabloSE <juancontas@protonmail.com>
2026-01-17 14:02:57 -0300

    Commit Message

---
+++ src/ExProposto05.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ExProposto05 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+
+        int [] [] A = new int [M] [N];
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                A[i][j] = sc.nextInt();
+            }
+        }
+
+        int [] [] B = new int [M] [N];
+        for (int i = 0; i < B.length; i++) {
+            for (int j = 0; j < B[i].length; j++) {
+                B[i][j] = sc.nextInt();
+            }
+        }
+
+        int [] [] C = new int [M] [N];
+        for (int i = 0; i < C.length; i++) {
+            for (int j = 0; j < C[i].length; j++) {
+                C[i][j] = A[i][j] + B[i][j];
+                System.out.print(C[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
