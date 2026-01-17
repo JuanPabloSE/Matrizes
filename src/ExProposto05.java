a517292e760b729531302ca7f47c61a6da5a05a3
JuanPabloSE <juancontas@protonmail.com>
2026-01-17 14:05:05 -0300

    Commit Message

--- src/ExProposto05.java
+++ src/ExProposto05.java
@@ -7,7 +7,7 @@ public class ExProposto05 {
         int M = sc.nextInt();
         int N = sc.nextInt();
 
-        int [] [] A = new int [M] [N];
+        int [] [] A = new int [M] [N]; // MATRIZ (A)
 
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A[i].length; j++) {
@@ -15,14 +15,16 @@ public class ExProposto05 {
             }
         }
 
-        int [] [] B = new int [M] [N];
+        int [] [] B = new int [M] [N]; // MATRIZ (B)
+
         for (int i = 0; i < B.length; i++) {
             for (int j = 0; j < B[i].length; j++) {
                 B[i][j] = sc.nextInt();
             }
         }
 
-        int [] [] C = new int [M] [N];
+        int [] [] C = new int [M] [N]; // MATRIZ (C)
+
         for (int i = 0; i < C.length; i++) {
             for (int j = 0; j < C[i].length; j++) {
                 C[i][j] = A[i][j] + B[i][j];
